7fa52b64396816c52ee79f78e83b86f3e2975e7d
Dauntekw <61566998+Dauntekw@users.noreply.github.com>
2022-02-10 16:06:48 -0800

    Commented anim_controller files
    Added comments to anim_controller.c and anim_controller.h

--- anim_controller.c
+++ anim_controller.c
@@ -1,34 +1,57 @@
+//---------------------------------------------------------
+// file:	anim_controller.c
+// author:	Daunte Williamson
+// email:	daunte.williamson@digipen.edu
+//
+// brief:	File that contains all core functions for
+// drawing animations onto the worldspace.
+//
+// documentation link:
+// https://github.com/DigiPen-Faculty/CProcessing/wiki
+//
+// Copyright © 2020 DigiPen, All rights reserved.
+//---------------------------------------------------------
+
 #include "cprocessing.h"
 #include "anim_controller.h"
 
-
+/*Display animation in worldspace. Returns new copy of passed animation after drawing current frame.*/
 struct Animation playAnimation(struct Animation anim, float xPos, float yPos)
 {
-  struct Animation newAnim = anim;
+  struct Animation newAnim = anim; /*Copy of passed animation argument.*/
 
-  int startCol = anim.startFrame;
-  int currentRow = anim.currentRow;
-  int currentCol = anim.currentFrame;
-  int endCol = anim.endFrame;
+  int startCol = anim.startFrame; /*Starting column for animation*/
+  int currentRow = anim.currentRow; /*Current row of animation being drawn*/
+  int currentCol = anim.currentFrame; /*Current column of animation being drawn*/
+  int endCol = anim.endFrame; /*Ending column of animation being drawn*/
 
+  /*Draw current frame of animation to worldspace*/
   DrawSpriteFrame(anim.file, anim.frameWidth, anim.frameHeight, currentRow, currentCol, xPos, yPos, anim.opacity);
   
+  /*Check if ready to advance frame in animation*/
   if (anim.currentStep <= 0)
   {
+    /*
+      currentStep is 0, increase current frame if next frame is less than or equal to ending column.
+      Reset currentCol if value is greater than ending column
+    */
     currentCol = (currentCol + 1 <= endCol) ? currentCol + 1 : (anim.loop) ? startCol : currentCol;
-    newAnim.currentStep = newAnim.frameRate;
+
+    newAnim.currentStep = newAnim.frameRate; /*Reset currentStep value to equal frameRate*/
+
+    /*Pass currentRow and currentFrame values to new copy of passed animation*/
     newAnim.currentRow = currentRow;
     newAnim.currentFrame = currentCol;
   }
   else 
   {
-    newAnim.currentStep = newAnim.currentStep - 1;
+    newAnim.currentStep = newAnim.currentStep - 1; /*currentStep is greater than 0, decrease by 1*/
   }
-  
 
-  return newAnim;
+  return newAnim; /*Return new copy of animation with updated values*/
 }
 
+/*Draw current frame of animation to worldspace using CP_Image_DrawSubImage*/
 void DrawSpriteFrame(CP_Image file, float width, float height, int row, 
   int col, float xPos, float yPos, int alpha) 
 {
--- anim_controller.h
+++ anim_controller.h
@@ -1,21 +1,29 @@
 #pragma once
 #include "cprocessing.h"
+/* 
+Author:       Daunte Williamson
+Email:        daunte.williamson@digipen.edu
+Description:  Header file for anim_controller.c
+              Contains definition for Animation Struct,
+              playAnimation(), DrawSpriteFrame()
+*/
 
+/*Struct for specific animations.*/
 struct Animation
 {
-  CP_Image file;
-  float frameWidth;
-  float frameHeight;
-  int startRow;
-  int startFrame;
-  int currentRow;
-  int currentFrame;
-  int endRow;
-  int endFrame;
-  int frameRate;
-  int currentStep;
-  int loop;
-  int opacity;
+  CP_Image file; /*CP file path for image resource*/
+  float frameWidth; /*Width of animation frame in pixels*/
+  float frameHeight; /*Height of animation frame in pixels*/
+  int startRow; /*0 indexed - starting row for animation on image file*/
+  int startFrame; /*0 indexed - starting column for first frame in animation*/
+  int currentRow; /*0 indexed - current row of image being drawn*/
+  int currentFrame; /*0 indexed - current column of image being drawn*/
+  int endRow; /*0 indexed - ending row for animation*/
+  int endFrame; /*0 indexed - ending column for animation*/
+  int frameRate; /*How many delay frames(steps) that must pass before next frame is drawn to scene*/
+  int currentStep; /*Decreasing counter for frameRate. 0 = move to next frame*/
+  int loop; /*Boolean - 1 = loop the animation, 0 = play animation only once; hold on end frame*/
+  int opacity; /*Value for alpha channel. 0 = invisible, 255 = completely visible*/
 };
 
 struct Animation playAnimation(struct Animation anim, float xPos, float yPos);
